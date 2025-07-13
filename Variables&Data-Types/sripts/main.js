167d51dfb60fa8b03f345c435e41ddfdb3d1f4ac
UTTHUNGADC\gokul.p <Gokul.p@utthunga.com>
2025-07-13 13:02:13 +0530

    JS file update

--- Variables&Data-Types/sripts/main.js
+++ Variables&Data-Types/sripts/main.js
@@ -24,3 +24,35 @@ let Result_with_not_operater = !a; // false, because it negates the true value
 console.log('!a -----> ' + Result_with_not_operater); // Output: false
 let result_with_adding_operater = a + b + a; // 1 + 0 + 1 = 2, because true is treated as 1 and false as 0
 console.log('a + b + a -----> ' + result_with_adding_operater); // Output: 2
+
+
+// variable Scope and types
+
+
+// let 
+let x1 = 10;
+if (true) {
+    let x1 = 20; // This x1 is different from the outer x1
+    console.log('Inside block, x1: ' + x1); // Output: 20
+    let x2 = 25; // This x2 is block-scoped
+    console.log('Inside block, x2: ' + x2); // Output: 25
+}
+x1 = 30; // This modifies the outer x1
+console.log('Outside block, x1: ' + x1); // Output: 30
+// let x1 = 40; // This will cause an error because x1 is already declared in the outer scope
+// console.log('Outside block, x2: ' + x2); // Output: undefined, because x2 is not accessible outside the block
+
+// var 
+var x3 = 10;
+if (true) {
+    var x3 = 20; // This x2 is the same as the outer x3
+    console.log('Inside block, x3: ' + x3); // Output: 20
+    var x4 = 25; // This redeclares x4 in the same scope
+    console.log('Inside block, x4: ' + x4); // Output: 25
+}
+x3 = 30; // This modifies the x2
+console.log('Outside block, x3: ' + x3); // Output: 30
+console.log('Outside block, x4: ' + x4); // Output: 25, because x3 is hoisted and accessible outside the block
+// var x2 = 40; // This will not cause an error, but it is not recommended to redeclare variables
+var x3 = 40; // This will not cause an error, but it is not recommended to redeclare variables
+console.log('Outside block, x3: ' + x3); // Output: 40
